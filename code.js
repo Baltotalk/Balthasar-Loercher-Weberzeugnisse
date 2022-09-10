443a1fbcab6e868a3b2107011542cc82e3a068e3
Balthasar Lörcher <balthasarlorcher@gmail.com>
2022-09-10 22:46:31 +0200

    menü

---
+++ code.js
@@ -0,0 +1,10 @@
+/* Toggle between adding and removing the "responsive" class to the navbar when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myNavbar");
+    if (x.className === "navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar";
+    }
+  }
+  
