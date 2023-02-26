2a987bd2294bd0196c9ca0f75679ba3672424b62
Baltotalk <balthasarlorcher@gmail.com>
2023-02-27 00:25:12 +0100

    27.02.

--- code.js
+++ code.js
@@ -11,7 +11,7 @@ function myFunction() {
 
 
   // Set the date we're counting down to
-var countDownDate = new Date("Jan 1, 2023 ").getTime();
+var countDownDate = new Date("Mar 1, 2023 ").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -39,6 +39,8 @@ var x = setInterval(function() {
   }
 }, 1000);
   
+
+//--------------------------------------------------------------------------------------
 
 
 
