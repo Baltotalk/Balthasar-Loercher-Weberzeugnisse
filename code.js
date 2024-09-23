63647692b9144f4cc7cb6830da11bde1359c0afb
Baltotalk <96119710+Baltotalk@users.noreply.github.com>
2024-09-23 10:48:38 +0200

    Update code.js

--- code.js
+++ code.js
@@ -11,7 +11,7 @@ function myFunction() {
 
 
   // Set the date we're counting down to
-var countDownDate = new Date("Mar 1, 2023 ").getTime();
+var countDownDate = new Date("Dec 1, 2024 ").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
