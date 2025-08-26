6e9f6e5c540dfb3cfd5dc1dcc27a9814af47a81f
Baltotalk <96119710+Baltotalk@users.noreply.github.com>
2025-08-26 13:53:25 +0200

    Updated Countdown to Timelapse 3.0

--- code.js
+++ code.js
@@ -11,7 +11,7 @@ function myFunction() {
 
 
   // Set the date we're counting down to
-var countDownDate = new Date("Oct 1, 2025 ").getTime();
+var countDownDate = new Date("Oct 22, 2025 ").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
